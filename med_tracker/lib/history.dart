6c3c77be5429491638e59d49ee94608796bdf7b6
Stephan Raab <sarcartist@gmail.com>
2022-12-18 13:00:18 -0500

    get tabbar working and importing data from pages

---
+++ med_tracker/lib/history.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HistoryPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:
+          Center(child: Container(child: Text('This is from HISTORY Page!!'))),
+    );
+  }
+}
---
+++ med_tracker/lib/inventory.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class InventoryPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Container(child: Text('This is from INVENTORY Page!!'))),
+    );
+  }
+}
